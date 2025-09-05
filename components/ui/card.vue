26aae7c267336e01f89f778be808a5173fb3a188
rstubryan <restubumirr@gmail.com>
2025-09-05 19:57:54 +0700

    feat: add card component and integrate it into the home page

---
+++ components/ui/card.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  link: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  className: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <NuxtLink
+    :href="link"
+    :class="`block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ${className}`"
+  >
+    <h5
+      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+    >
+      {{ title }}
+    </h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      {{ description }}
+    </p>
+  </NuxtLink>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
+<script setup>
+import Card from "../components/ui/card.vue";
+</script>
+
 <template>
   <div>
     <h1>Welcome to the Home Page</h1>
+    <Card
+      link="/about"
+      title="About Us"
+      description="Learn more about our team and mission."
+      className="mb-4"
+    />
   </div>
 </template>
