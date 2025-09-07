3456216ab10a94a614686779f9f0d85188a89915
rstubryan <restubumirr@gmail.com>
2025-09-07 10:44:57 +0700

    feat: refactor card component to improve structure and accessibility

--- components/ui/card.vue
+++ components/ui/card.vue
@@ -20,17 +20,17 @@ defineProps({
 </script>
 
 <template>
-  <NuxtLink
-    :href="link"
+  <div
     :class="`block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ${className}`"
   >
-    <h5
+    <NuxtLink
+      :href="link"
       class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
     >
       {{ title }}
-    </h5>
+    </NuxtLink>
     <p class="font-normal text-gray-700 dark:text-gray-400">
       {{ description }}
     </p>
-  </NuxtLink>
+  </div>
 </template>
